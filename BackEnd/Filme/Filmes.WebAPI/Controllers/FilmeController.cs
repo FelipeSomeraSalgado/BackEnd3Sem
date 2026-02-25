4963a6054d534814fde51fc165e1f54331fecf73
FELIPE SOMERA SALGADO <46725344801@info.local>
2026-02-25 16:49:13 -0300

    Atualização da API

---
+++ BackEnd/Filme/Filmes.WebAPI/Controllers/FilmeController.cs
@@ -0,0 +1,89 @@
+﻿using Filmes.WebAPI.Interfaces;
+using Filmes.WebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Filmes.WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FilmeController : ControllerBase
+{
+    private readonly IFilmeRepository _filmeRepository;
+    public FilmeController(IFilmeRepository filmeRepository)
+    {
+        _filmeRepository = filmeRepository;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        try
+        {
+            return Ok(_filmeRepository.Listar());
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(Guid Id)
+    {
+        try
+        {
+
+            return Ok(_filmeRepository.BuscarPorId(Id));
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+
+    [HttpPost]
+    public IActionResult Post(Filme novoFilme)
+    {
+        try
+        {
+            _filmeRepository.Cadastrar(novoFilme);
+            return StatusCode(201);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpPut("{id}")]
+
+    public IActionResult Put(Guid id, Filme filme)
+    {
+        try
+        {
+            _filmeRepository.AtualizarIdUrl(id, filme);
+            return NoContent();
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpPut]
+    public IActionResult PutBody(Filme filme)
+    {
+        try
+        {
+            _filmeRepository.AtualizarIdCorpo(filme);
+            return NoContent();
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+
+    }
+}
